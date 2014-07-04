9cd60866eb3b01d3aa2de3b769b607a0eaa56ffe
Administrator <Administrator@Lomanstop.local>
2014-07-03 20:56:41 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/libs/cocos2d-iphone/Creature.h
@@ -0,0 +1,21 @@
+//
+//  Creature.h
+//  cocos2d-ios
+//
+//  Created by Administrator on 7/3/14.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+- (id)initCreature;
+@end
---
+++ GameOfLife.spritebuilder/Source/libs/cocos2d-iphone/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  cocos2d-ios
+//
+//  Created by Administrator on 7/3/14.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
