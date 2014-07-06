745dd668f33b52b5d19d8d5fccb953b5bee42735
Administrator <Administrator@Lomanstop.local>
2014-07-05 22:44:54 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/libs/cocos2d-iphone/Grid.m
+++ GameOfLife.spritebuilder/Source/libs/cocos2d-iphone/Grid.m
@@ -155,7 +155,7 @@ int column = 0;
 - (void)updateCreatures
 {
   
-    {int numAlive = 0;
+    {int numAlive = 3;
         // iterate through the rows
         // note that NSArray has a method 'count' that will return the number of elements in the array
         for (int i = 0; i < [_gridArray count]; i++)
